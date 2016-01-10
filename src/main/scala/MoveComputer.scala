0e1e8f6b0833f7bb141b211da2252fc1e8c651f9
Martin Laporte <mlaporte@coveo.com>
2016-01-10 14:02:16 -0500

    Add some proximity scoring.

--- src/main/scala/MoveComputer.scala
+++ src/main/scala/MoveComputer.scala
@@ -39,31 +39,26 @@ class MoveComputer(input: Input) {
     val pathsToTaverns = scoredPathsTo(_ == Tile.Tavern)
 
     pathsToTaverns.headOption match {
-      case Some(path) if path.path.length == 1 && hero.life < MoveComputer.THIRST_OF_OPPORTUNITY_THRESHOLD =>
-        Seq(MoveAndScore(s"Why not take a drink while I'm here?", path.path.firstMove, Int.MaxValue))
+      case Some(path) if path.path.length == 1 && hero.life < MoveComputer.DRINK_OF_OPPORTUNITY_THRESHOLD =>
+        Seq(MoveAndScore(s"Why not take a drink while I'm here?", path.path.firstMove, MoveComputer.DRINK_OF_OPPORTUNITY_SCORE))
       case _ =>
         Nil
     }
   }
 
   private def getGrabMineMoves: Seq[MoveAndScore] = {
-    val pathsToAvailableMines = scoredPathsTo({
-      case mine: Tile.Mine => mine.heroId.isEmpty || !mine.heroId.contains(hero.id)
-      case _ => false
-    }, minimumLife = MoveComputer.MINIMUM_LIFE_FOR_MINE)
+    val pathsToAvailableMines = scoredPathsTo(isAvailableMine, minimumLife = MoveComputer.GRAB_MINE_MINIMUM_LIFE)
 
     pathsToAvailableMines.map { path =>
-      val score = -path.path.cost
+      val score = MoveComputer.GRAB_MINE_SCORE - (path.path.cost * MoveComputer.GRAB_MINE_COST_FACTOR) + computeLocationScore(path.pos)
       MoveAndScore(s"Going to mine at ${path.pos}", path.path.firstMove, score)
     }
   }
 
   private def getFallbackMoves: Seq[MoveAndScore] = {
-    scoredPathsTo(_ == Tile.Tavern).headOption match {
-      case Some(path) =>
-        Seq(MoveAndScore("Go to tavern since I have nothing to do", path.path.firstMove, Float.MinValue))
-      case None =>
-        Nil
+    scoredPathsTo(_ == Tile.Tavern).map { path =>
+      val score = MoveComputer.BORED_DRINK_SCORE + computeLocationScore(path.pos)
+      MoveAndScore(s"Go to tavern ${path.pos} since I have nothing to do", path.path.firstMove, score)
     }
   }
 
@@ -79,6 +74,31 @@ class MoveComputer(input: Input) {
       .sortBy(_.path.cost)
   }
 
+  private def computeLocationScore(pos: Pos): Float = {
+    var score = 0f
+
+    score += computeProximityScore(pos, isAvailableMine) * MoveComputer.LOCATION_MINE_PROXIMITY_FACTOR
+    score += computeProximityScore(pos, _ == Tile.Tavern) * MoveComputer.LOCATION_TAVERN_PROXIMITY_FACTOR
+
+    score
+  }
+
+  private def computeProximityScore(pos: Pos,
+                                    matcher: Tile => Boolean): Float = {
+
+    val allPaths = allTiles
+      .filter(x => matcher(x.tile))
+      .flatMap(tile => pathFinder.findPath(pos, tile.pos).map(TileAndPath(tile.tile, tile.pos, _)))
+
+    allPaths.map(_.path.cost) match {
+      case Nil =>
+        0
+      case costs =>
+        val max = costs.max.toFloat
+        costs.map(max / _).sum
+    }
+  }
+
   private def allTiles: Seq[TileAndPos] = {
     (0 until board.size).flatMap { y =>
       (0 until board.size).flatMap { x =>
@@ -87,13 +107,26 @@ class MoveComputer(input: Input) {
       }
     }
   }
+
+  private def isAvailableMine(tile: Tile): Boolean = tile match {
+    case mine: Tile.Mine => mine.heroId.isEmpty || !mine.heroId.contains(hero.id)
+    case _ => false
+  }
 }
 
 object MoveComputer {
   val MINE_COST = 25
   val MAXIMUM_LIFE = 100
 
-  val THIRST_THRESHOLD = MINE_COST
-  val THIRST_OF_OPPORTUNITY_THRESHOLD = 90
-  val MINIMUM_LIFE_FOR_MINE = MINE_COST + 1
+  val DRINK_OF_OPPORTUNITY_SCORE = 10000000f
+  val DRINK_OF_OPPORTUNITY_THRESHOLD = 90
+
+  val GRAB_MINE_SCORE = 10000f
+  val GRAB_MINE_COST_FACTOR = 5f
+  val GRAB_MINE_MINIMUM_LIFE = MINE_COST + 1
+
+  val BORED_DRINK_SCORE = -100000f
+
+  val LOCATION_MINE_PROXIMITY_FACTOR = 5f
+  val LOCATION_TAVERN_PROXIMITY_FACTOR = 5f
 }
